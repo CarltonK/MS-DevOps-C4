1ab252843da1d5566ab5e0aae52e038180023edd
Mark Carlton <mcarlton33@gmail.com>
2023-03-23 09:41:24 +0300

    Dockerizing applications

---
+++ Week-7A/README.md
@@ -0,0 +1,6 @@
+[ENV]
+
+1. Create a .env file
+2. Paste environment variables into the file in the format X=Y
+
+@Ole cloned it.
---
+++ Week-7A/index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const teamRoutes = require('./routes/teams');
+const indexRoutes = require('./routes/index');
+
+// Variables
+const port = process.env.PORT || 3000;
+const databaseUrl = process.env.DATABASE_URL;
+
+// Connect to the database
+mongoose.connect(databaseUrl);
+const database = mongoose.connection;
+
+database.on('error', (error) => {
+    console.error(error);
+});
+
+database.once('connected', () => {
+    console.log('Database connected');
+});
+
+// Create an express app
+const app = express();
+
+app.use(express.json());
+app.use('/', indexRoutes);
+app.use('/teams', teamRoutes);
+
+app.listen(port, () => {
+    console.log(`Server is running on PORT ${port}`);
+});
