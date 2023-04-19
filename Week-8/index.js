3283a351f9436262b1d5a8aad928c9be262fad20
Mark Carlton <mcarlton33@gmail.com>
2023-04-19 09:55:41 +0300

    jest testing

---
+++ Week-8/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    return res.status(200).json({ running: true });
+});
+
+let server = app.listen(PORT, () => {
+    console.log(`Your application is running on http://localhost:${PORT}`);
+});
+
+module.exports = server;
