b27beaf78913006e144a9ea1d370fe180d899f90
Mark Carlton <mcarlton33@gmail.com>
2023-05-18 10:50:40 +0300

    TF

---
+++ Week-9/terraform/docker.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    docker = {
+      source = "terraform-providers/docker"
+    }
+  }
+}
+
+provider "docker" {}
+
+resource "docker_image" "busybox" {
+  name = "busybox:latest"
+}
+
