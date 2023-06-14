cfaf4a903c2524e5d5c9b05e88c22820707d5e4b
Mark Carlton <mcarlton33@gmail.com>
2023-06-14 10:08:50 +0300

    local k8s with env vars

--- Week-10/Kubernetes/index.js
+++ Week-10/Kubernetes/index.js
@@ -6,6 +6,8 @@ const PORT = 3000;
 app.use(express.json());
 
 app.get('/', (req, res) => {
+    const environment = process.env.NODE_ENV;
+    console.log(`This request is running on the ${environment} environment`);
     res.status(200).json({ hello: 'world' });
 });
 
